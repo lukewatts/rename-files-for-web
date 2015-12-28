46fea73a3bd49d09bb0b924ec06966ff2ce9ce6d
Luke Watts <luke@luke-watts.com>
2015-12-28 01:56:16 +0000

    Adding rename-for-web.py

---
+++ rename-for-web.py
@@ -0,0 +1,19 @@
+import os
+
+curdir = os.getcwd()
+fullpath = os.path.abspath(curdir) + '\\'
+filenames = os.listdir(curdir)
+# Loop through files in the current directory
+for filename in filenames:
+    # Make sure we're dealing with files
+    if os.path.isfile(filename):
+        # Replace spaces with hyphens (-)
+        hyphenated_filename = filename.replace(' ', '-')
+
+        # Rename files using .lower()
+        new_filename = hyphenated_filename.lower()
+        # Combine with path to make full pathname
+        path_filename = fullpath + filename
+
+        # Rename the file
+        os.rename(path_filename, new_filename)
